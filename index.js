6909abab32704ba45d6d952ff4ca59fb9a8a719c
abdullaevbaxrom0000 <abdullaev_baxrom@internet.ru>
2025-03-15 17:46:23 +0500

    Update packagejson4

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors({
 
 
 app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+  res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none');
   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   next();
